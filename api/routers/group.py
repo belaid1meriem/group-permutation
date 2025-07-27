5c59516aa11218fc3b83c35d21dadf97da83a89b
belaid1meriem <mm_belaid@esi.dz>
2025-07-27 16:05:17 +0100

    Added more validation and error handling

--- api/routers/group.py
+++ api/routers/group.py
@@ -1,5 +1,7 @@
 from typing import List
 from fastapi import APIRouter, Body
+
+from ..schemas.group import GroupPermutationRequest
 from ..schemas.student import Student, StudentResponse
 from ..services.group_service import process_group_permutation
 
@@ -7,31 +9,10 @@ router = APIRouter()
 
 @router.post(
     "/group_permutation",
+    # request_model=GroupPermutationRequest,
     response_model=List[StudentResponse],
     summary="Generate student group permutations",
     description="Takes a list of students and groups to process their movement.",
 )
-def group_permutation(
-    students_list: List[Student] = Body(
-        ...,
-        example=[
-            {
-                "matricule": "22/0008",
-                "fromG": "G01",
-                "toG": "G02",
-                "date": "2025-03-03T10:39:58.392260"
-            },
-            {
-                "matricule": "22/0010",
-                "fromG": "G03",
-                "toG": "G04",
-                "date": "2025-03-03T11:15:20.123456"
-            }
-        ]
-    ),
-    group_list: List[str] = Body(
-        ...,
-        example=["G01", "G02", "G03", "G04"]
-    )
-):
-    return process_group_permutation(students_list, group_list)
+def group_permutation(request: GroupPermutationRequest):
+    return process_group_permutation(request.students_list, request.group_list)
